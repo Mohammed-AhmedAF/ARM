5334fecc441daa28de1bd5f5763f9f508782c74c
Mohamed Ahmed Abd Al-Fattah Mahmoud <mohamedstarwars@gmail.com>
2018-07-03 23:47:41 +0200

    Without the library provided by Texas

--- tiva-c/Blinky/main.c
+++ tiva-c/Blinky/main.c
@@ -1,28 +1,26 @@
-//
-// Author: Mohammed Ahmed Abd Al-Fattah
-// Project: First Tiva-c, from udemy course
-//
-//
+#define RCGCGPIO *((unsigned int *) 0x400FE608)
+#define GPIOF_DIR *((unsigned int *) 0x40025400)
+#define GPIOF_DEN *((unsigned int *) 0x4002551C)
+#define GPIOF_DATA *((unsigned int *) 0x400253FC)
 
 
-#include "TM4C123.h"                    // Device header
-
-#define GREEN (1U<<3) //Green LED is connected to PIN3 
+#define GREEN_LED (1U<<3)
 
 void delay(int time); //A delay function
 
-int main(void) {
-	SYSCTL->RCGCGPIO |= (1U<<5); //Connecting the clock to PORTF
-	//The LED is connected to that port.
-	GPIOF->DIR |=GREEN;
-	GPIOF->DEN |=GREEN;
-	GPIOF->DATA |=GREEN;
+int main(void)
+{
+    RCGCGPIO |= (1U<<5);
+    GPIOF_DIR |= GREEN_LED;
+    GPIOF_DEN |= GREEN_LED;
 
-	while(1) {
-		delay(1000000);
-		GPIOF->DATA ^= GREEN;
-		delay(2000000);
-	}
+    while(1) {
+				delay(2000000);
+		    GPIOF_DATA ^= GREEN_LED;
+				delay(2000000);
+
+		}
+
 	return 0;
 }
 
@@ -30,5 +28,5 @@ void delay(int time) {
 	int delay = 0;
 	while (delay < time) {
 		delay++;
-	}
+}
 }
