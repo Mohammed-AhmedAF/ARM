be069b2c4b67f38afdad5339b8b2aaa58b7af99e
Mohamed Ahmed Abd Al-Fattah Mahmoud <mohamedstarwars@gmail.com>
2018-07-16 23:10:11 +0200

    MDIO_interface.h: Adding functions declaration

--- lib/MDIO_interface.h
+++ lib/MDIO_interface.h
@@ -1,1 +1,9 @@
+#ifndef _MDIO_INTERFACE_H
+#define _MDIO_INTERFACE_H
 
+
+void MDIO_vidInit(void);
+void MDIO_vidSetPinValue(u8 u8PortNum, u8 u8PinNum, u8 u8Value);
+void MDIO_u8GetPinValue(u8 u8PortNum, u8 u8PinNum);
+
+#endif
