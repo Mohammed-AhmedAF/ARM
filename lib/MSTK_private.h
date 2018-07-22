a191c608c53103a1d3acdc364557dd22b54f4268
Mohamed Ahmed Abd Al-Fattah Mahmoud <mohamedstarwars@gmail.com>
2018-07-22 18:21:24 +0200

    MSTK: SysTick

---
+++ lib/MSTK_private.h
@@ -0,0 +1,14 @@
+#ifndef _MSTK_PRIVATE_H
+#define _MSTK_PRIVATE_H
+
+typedef struct {
+	u32 CTRL;
+	u32 LOAD;
+	u32 VAL;
+	u32 CALIB;
+
+}SysTick_Type;
+
+#define MSTK ((u32*) 0xE000E010)
+
+#endif
---
+++ lib/MSTK_program.c
@@ -0,0 +1,22 @@
+#include "LSTD_TYPES.h"
+#include "Macros.h"
+#include "MSTK_private.h"
+#include "MSTK_interface.h"
+
+void (*MSTK_CallBack) (void); /*Pointer to function*/
+
+void MSTK_vidInit(void) {
+	MSTK->CTRL = 3;
+	MSTK->VAL = 1000000;
+	MSTK->LOAD = 1000000;
+}
+
+void MSTK_vidSetCallBack(void (*ptrFunc) (void)) {
+	MSTK_CallBack = ptrFunc;
+}
+
+
+/*Interrupt Handler*/
+void SysTick_Handler(void) {
+	MSTK_CallBack(void);
+}
