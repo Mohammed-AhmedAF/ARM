ca7f654ec80ef0c5e2149aa06cf84d9efbc96b0e
Mohamed Ahmed Abd Al-Fattah Mahmoud <mohamedstarwars@gmail.com>
2018-08-02 20:17:16 +0200

    Adding comments

--- projects/ExternalInterrupt/main.c
+++ projects/ExternalInterrupt/main.c
@@ -22,9 +22,9 @@ void func(void) {
 
 void main(void) {
 	MRCC_vidInit();
-	MRCC_vidEnableClock(BUS_APB2,PERIPHERAL_GPIOA);
+	MRCC_vidEnableClock(BUS_APB2,PERIPHERAL_GPIOA); /*To enable clock for input pin*/
 	MRCC_vidEnableClock(BUS_APB2,PERIPHERAL_GPIOB);
-	MRCC_vidEnableClock(BUS_APB2,PERIPHERAL_AFIO);
+	MRCC_vidEnableClock(BUS_APB2,PERIPHERAL_AFIO); /*To enable clock for external interrupt*/
 
 	MDIO_vidInit();
 
