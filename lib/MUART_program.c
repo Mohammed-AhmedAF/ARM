038195f5722c1b901a2e5c54422cec5b47c55a92
Mohamed Ahmed Abd Al-Fattah Mahmoud <mohamedstarwars@gmail.com>
2018-08-06 13:49:59 +0200

    UART: disabling clock and adding comment explaing why

--- lib/MUART_program.c
+++ lib/MUART_program.c
@@ -1,30 +1,32 @@
-/*
- * Author: Mohammed Ahmed Abd Al-Fattah
- * Puporse: UART driver for STM32F103c8t6 MCU
- * Date: 29-7-2018
- */
 #include "LSTD_TYPES.h"
 #include "Macros.h"
 #include "MUART_private.h"
 #include "MUART_interface.h"
 
+/*
+ * CLK enabled for USART, if enabled receive will not work
+ * with UART-USB dongle
+ *
+ */
+
 void MUART_vidInit(void) {
-	SET_BIT(MUART->CR1,13); /*UART Enable*/
 	CLEAR_BIT(MUART->CR1,12); /*Word length*/
 	CLEAR_BIT(MUART->CR1,11); /*UART Wake Up method*/
 	CLEAR_BIT(MUART->CR1,10); /*Parity Control*/
 	CLEAR_BIT(MUART->CR1,8); /*Interrupt is inhibited*/
 	SET_BIT(MUART->CR1,3); /*Transmitter is enabled*/
 	SET_BIT(MUART->CR1,2); /*Receiver is enabled*/
-	SET_BIT(MUART->CR2,11); /*Clock is enabled*/
+	CLEAR_BIT(MUART->CR2,11); /*Clock is disabled*/
 	MUART->BRR = 0x341; /*Baudrate*/
 
 	SET_BIT(MUART->GTPR,3); /*Setting prescaler*/
+	SET_BIT(MUART->CR1,13); /*UART Enable*/
 }
 
 void MUART_vidSendByte(u8 u8ByteCpy) {
 	MUART->DR = u8ByteCpy;
 	while (GET_BIT(MUART->SR,7) == 0); /*Checking Transmission Data Register Empty flag*/
+
 }
 
 u8 MUART_u8ReceiveByte(void) {
