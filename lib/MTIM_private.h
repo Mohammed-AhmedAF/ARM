7dc5785486dded324c6c0ce0ed0686dc545f90c8
Mohamed Ahmed Abd Al-Fattah Mahmoud <mohamedstarwars@gmail.com>
2018-10-24 12:37:36 +0200

    Timer: Adding definition of Advanced TimersÄ

--- lib/MTIM_private.h
+++ lib/MTIM_private.h
@@ -22,6 +22,29 @@ typedef struct {
 	volatile u32 DMAR;
 }Timer_Type;
 
+typedef struct {
+	volatile u32 CR1;
+	volatile u32 CR2;
+	volatile u32 SMCR;
+	volatile u32 DIER;
+	volatile u32 SR;
+	volatile u32 EGR;
+	volatile u32 CCMR1;
+	volatile u32 CCMR2;
+	volatile u32 CCER;
+	volatile u32 CNT;
+	volatile u32 PSC;
+	volatile u32 ARR;
+	volatile u32 RCR;
+	volatile u32 CCR1;
+	volatile u32 CCR2;
+	volatile u32 CCR3;
+	volatile u32 CCR4;
+	volatile u32 BDTR;
+	volatile u32 DCR;
+	volatile u32 DMAR;
+}Advanced_Timer_Type;
+
 #define MTIM2 ((Timer_Type*) 0x40000000)
 #define MTIM3 ((Timer_Type*) 0x40000400)
 #define MTIM4 ((Timer_Type*) 0x40000800)
