1b9abee09dbe22434d3a81ec40084ff1ce83375d
Mohammed Ahmed <mohamedstarwars@gmail.com>
2021-04-03 09:49:27 +0200

    UART example working

---
+++ tiva-c/UART2/SYSCNTRL_private.h
@@ -0,0 +1,9 @@
+#ifndef _SYSCNTRL_PRIVATE_H_
+#define _SYSCNTRL_PRIVATE_H_
+#define RCGCGPIO *((unsigned int *) 0x400FE608)
+#define RCGCUART *((unsigned int *) 0x400FE618)
+#define RCGCTIMER * ((unsigned int *) 0x400FE604)
+#define RCGCI2C * ((u32*) 0x400FE620)
+
+
+#endif
