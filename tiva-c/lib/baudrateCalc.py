311d352dfd84e83045218cb4731465a4eb348d2d
Mohammed Ahmed <4983317+Mohammed-AhmedAF@users.noreply.github.com>
2021-05-12 19:34:17 +0000

    Update baudrateCalc.py
    Checking entered value of baudrate

--- tiva-c/lib/baudrateCalc.py
+++ tiva-c/lib/baudrateCalc.py
@@ -1,7 +1,16 @@
 #Calculate the values of IBRD and FBRD for UART
 #of Tiva-C
 
-baudrate = 9600
+def askForBaudrate():
+  baudrate = input("Enter baudrate: ")
+  if baudrate.isnumeric():
+    return baudrate
+  else:
+    print("Wrong value")
+    askForBaudrate()
+    
+baudrate = askForBaudrate()
+  
 clkDiv = 16 #16 or 8 depending on HSE in UARTCTL
 sysClk = 16000000
 brd = sysClk / (clkDiv*baudrate)
