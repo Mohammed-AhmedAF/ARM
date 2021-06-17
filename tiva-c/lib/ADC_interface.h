b0aa436c06c29e8e0bc15b2dc518f9fc927e86f6
Mohammed Ahmed <mohamedstarwars@gmail.com>
2021-06-17 14:53:21 +0200

    Tiva-c: Using structures in ADC_vidInit()

--- tiva-c/lib/ADC_interface.h
+++ tiva-c/lib/ADC_interface.h
@@ -4,15 +4,45 @@
 typedef struct 
 {
 	u8 u8Sequencer;
+	u8 u8AnalogInput;
+	u8 u8InterrupEnabled;
+	u8 u8EventTrigger;
 } ADC_Config_t;
 
 
-void ADC0_vidInit(void);
-void ADC1_vidInit(void);
+void ADC0_vidInit(ADC_Config_t * ADC_Config);
+void ADC1_vidInit(ADC_Config_t * ADC_Config);
 void ADC_vidStartSampling(void);
 
 /*Macros*/
+
+/*Sequencer*/
+#define ADC_SEQUENCER_0 0
+#define ADC_SEQUENCER_1 1
+#define ADC_SEQUENCER_2 2
+#define ADC_SEQUENCER_3 3
+
+/*Event Triggers*/
 #define ADC_EVENT_TRIGGER_PROCESSOR 0
 #define ADC_EVENT_TRIGGER_CONTINUOUS 1
+#define ADC_EVENT_TRIGGER_EXTERNAL 4
+
+/*Interrupt*/
+#define ADC_INTERRUPT_DISABLED 0
+#define ADC_INTERRUPT_ENABLED 1
+
+/*Analog input*/
+#define ADC_ANALOGINPUT_0 0
+#define ADC_ANALOGINPUT_1 1
+#define ADC_ANALOGINPUT_2 2
+#define ADC_ANALOGINPUT_3 3
+#define ADC_ANALOGINPUT_4 4
+#define ADC_ANALOGINPUT_5 5
+#define ADC_ANALOGINPUT_6 6
+#define ADC_ANALOGINPUT_7 7
+#define ADC_ANALOGINPUT_8 8
+#define ADC_ANALOGINPUT_9 9
+#define ADC_ANALOGINPUT_10 10
+#define ADC_ANALOGINPUT_11 11
 
 #endif
--- tiva-c/lib/ADC_program.c
+++ tiva-c/lib/ADC_program.c
@@ -6,52 +6,115 @@
 
 volatile u32 u32ADCResult;
 
-void ADC0_vidInit(void)
+void ADC0_vidInit(ADC_Config_t * ADC_Config)
 {
 	/*Ensure disabling of sample sequencer*/
 	CLEAR_BIT(ADC0->ACTSS,3);
 	/*Proccessor Sample Sequence Initiate*/
 	
 	/*Event trigger*/
-	/*Processor*/
-	ADC0->EMUX = ~0xF<12;
-
+	/**/
+	switch(ADC_Config->u8EventTrigger)
+	{
+		case ADC_EVENT_TRIGGER_PROCESSOR:
+		ADC0->EMUX = ~0x0<12;
+		break;
+		case ADC_EVENT_TRIGGER_CONTINUOUS:
+		ADC0->EMUX = ~0xF<12;
+		break;
+	}
 	/*Selecting analog input*/
 	/*Analog input 2: PE1*/
-	ADC0->SSMUX3 = 2;
+	ADC0->SSMUX3 = ADC_Config->u8AnalogInput;
 	
 	ADC0->SSCTL3 |= 0x6;
 	
 	/*Enabling interrupt*/
-	SET_BIT(ADC0->IM,3);
-	
+	if (ADC_Config->u8InterrupEnabled)
+	{
+			SET_BIT(ADC0->IM,3);
+	}
+	else
+	{
+			CLEAR_BIT(ADC0->IM,3);
+
+	}
 	/*Enabling sample sequencer 3*/
 	SET_BIT(ADC0->ACTSS,3);
 
 	SET_BIT(ADC0->PSSI,3);
 }
 
-void ADC1_vidInit(void)
+void ADC1_vidInit(ADC_Config_t * ADC_Config)
 {
 	/*Ensure disabling of sample sequencer*/
-	CLEAR_BIT(ADC1->ACTSS,3);
-	/*Proccessor Sample Sequence Initiate*/
+	switch(ADC_Config->u8Sequencer)
+	{
+		case ADC_SEQUENCER_0:
+		CLEAR_BIT(ADC1->ACTSS,0);
+		break;
+		case ADC_SEQUENCER_1:
+			CLEAR_BIT(ADC1->ACTSS,1);
+		break;
+		case ADC_SEQUENCER_2:
+			CLEAR_BIT(ADC1->ACTSS,2);
+		break;
+		case ADC_SEQUENCER_3:
+		CLEAR_BIT(ADC1->ACTSS,3);
+		break;
+	}
+		/*Proccessor Sample Sequence Initiate*/
 	
 	/*Event trigger*/
 	/*Processor*/
-	ADC1->EMUX = ~0xF<12;
+		switch(ADC_Config->u8EventTrigger)
+	{
+		case ADC_EVENT_TRIGGER_PROCESSOR:
+		ADC1->EMUX = ~0x0<12;
+		break;
+		case ADC_EVENT_TRIGGER_CONTINUOUS:
+		ADC1->EMUX = ~0xF<12;
+		break;
+	}
 
 	/*Selecting analog input*/
 	/*Analog input 2: PE1*/
-	ADC1->SSMUX3 = 2;
-	
+	switch(ADC_Config->u8Sequencer)
+	{
+		case ADC_SEQUENCER_0:
+		ADC1->SSMUX0 = ADC_Config->u8AnalogInput;
+		break;
+		case ADC_SEQUENCER_1:
+		ADC1->SSMUX1 = ADC_Config->u8AnalogInput;
+		break;
+		case ADC_SEQUENCER_2:
+		ADC1->SSMUX2 = ADC_Config->u8AnalogInput;
+		break;
+		case ADC_SEQUENCER_3:
+		ADC1->SSMUX3 = ADC_Config->u8AnalogInput;
+		break;
+	}
 	ADC1->SSCTL3 |= 0x6;
 	
 	/*Enabling interrupt*/
 	SET_BIT(ADC1->IM,3);
 	
 	/*Enabling sample sequencer 3*/
-	SET_BIT(ADC1->ACTSS,3);
+		switch(ADC_Config->u8Sequencer)
+	{
+		case ADC_SEQUENCER_0:
+				SET_BIT(ADC1->ACTSS,0);
+		break;
+		case ADC_SEQUENCER_1:
+				SET_BIT(ADC1->ACTSS,1);
+		break;
+		case ADC_SEQUENCER_2:
+				SET_BIT(ADC1->ACTSS,2);
+		break;
+		case ADC_SEQUENCER_3:
+				SET_BIT(ADC1->ACTSS,3);
+		break;
+	}
 
 	SET_BIT(ADC1->PSSI,3);
 }
