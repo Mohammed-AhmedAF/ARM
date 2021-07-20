3baedfa0930320b2f2e639e98575504279c4c335
Mohammed Ahmed <mohamedstarwars@gmail.com>
2021-07-20 12:34:04 +0200

    Tiva-C: Stopwatch: Using UART to reset the timer

--- tiva-c/Examples/Stopwatch/APP_program.c
+++ tiva-c/Examples/Stopwatch/APP_program.c
@@ -12,6 +12,7 @@ volatile static u8 u8CountDirection = APP_COUNT_UP;
 
 void vidBlink(void)
 {
+
 	GPIO_vidTogglePin(GPIO_PORTF,GPIO_PIN1);
 	UART_vidSendNumber(UART0_vidSendByte,u8Hours);
 	UART0_vidSendByte(':');
@@ -20,6 +21,7 @@ void vidBlink(void)
 	UART_vidSendNumber(UART0_vidSendByte,u8Seconds);
 	UART0_vidSendByte('\n');
 	UART0_vidSendByte('\r');
+	
 	
 	if (u8CountDirection == APP_COUNT_UP)
 	{
@@ -60,6 +62,14 @@ void vidProcessCommand(void)
 	{
 			GPIO_vidTogglePin(GPIO_PORTF,GPIO_PIN2);
 
+	}
+	else if(u8Command == 'r')
+	{
+		vidResetStopWatch();
+	}
+	else if(u8Command == 'd')
+	{
+		vidCountDown();
 	}
 }
 
