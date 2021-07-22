6ecc6294651bc5d9546f8b0b91cf1622f3a86ec7
Mohammed Ahmed <mohamedstarwars@gmail.com>
2021-07-22 13:43:29 +0200

    Tiva-C: Adding vidInit by struct to TIMERS drivers

--- tiva-c/lib/TIMERS_program.c
+++ tiva-c/lib/TIMERS_program.c
@@ -10,7 +10,7 @@
 #include "TM4C123.h"                    // Device header
 
 
-void (*TIMERS_vidCallBack) (void);
+static void (*TIMERS_vidCallBack) (void);
 
 void TIMERS_vidPutFunction(void (*ptFun) (void)) {
 		TIMERS_vidCallBack = ptFun;
@@ -55,4 +55,88 @@ void TIMER0A_Handler(void) {
 		TIMERS_vidCallBack();
 	  GPTM0_ICR = 0x01; /*To clear interrupt flag*/
 
+}
+
+void TIMER0A_vidConfigInputCapt(TIMERConfig_t * TIMERConfig)
+{
+
+		CLEAR_BIT(GPTM0_CTL,0);
+		
+		switch(TIMERConfig->u8Config)
+		{
+			case TIMER_CONFIG_1632_16BIT:
+				GPTM0_CFG |= 0x04;
+				break;
+			case TIMER_CONFIG_3264_32BIT:
+				GPTM0_CFG |= 0x04;
+				break;
+			
+		}
+		/*Timer0 A mode*/
+		switch(TIMERConfig->u8TimerAMode)
+		{
+			case TIMER_TIMERA_MODE_ONESHOOT:
+				GPTM0_TAMR |= 0x01;
+			break;
+			case TIMER_TIMERA_MODE_PERIODIC:
+				GPTM0_TAMR |= 0x02;
+			break;
+			case TIMER_TIMERA_MODE_CAPTURE:	
+		GPTM0_TAMR |= 0x03;
+			break;
+		}
+		
+		/*Capture mode*/
+		switch(TIMERConfig->u8TimerACaptMode)
+		{
+			case TIMER_TIMERA_CAPTMODE_EDGECOUNT:
+				CLEAR_BIT(GPTM0_TAMR,2);
+			break;
+			case TIMER_TIMERA_CAPTMODE_EDGETIMER:
+				SET_BIT(GPTM0_TAMR,2);
+			break;
+		}
+		/*Count direction*/
+		switch(TIMERConfig->u8TimerACountDir)
+		{
+			case TIMER_TIMERA_COUNTDIR_DOWN:
+				CLEAR_BIT(GPTM0_TAMR,4);
+				break;
+			case TIMER_TIMERA_COUNTDIR_UP:
+				SET_BIT(GPTM0_TAMR,4);
+			break;
+		}
+		
+		/*Event mode*/
+		switch(TIMERConfig->u8TimerAEventMode)
+		{
+			case TIMER_TIMERA_EVENTMODE_POSITIVE:
+				TIMER0->CTL &= ~(1<<3)|~(1<<2);   /* capture rising edges on PB6 pin */
+				break;
+			case TIMER_TIMERA_EVENTMODE_NEGATIVE:
+				CLEAR_BIT(GPTM0_CTL,3);
+				SET_BIT(GPTM0_CTL,2);
+			break;
+			case TIMER_TIMERA_EVENTMODE_BOTHEDGES:
+				SET_BIT(GPTM0_CTL,3);
+				SET_BIT(GPTM0_CTL,2);
+			break;
+		}
+    /* enable Timer0A */
+		SET_BIT(GPTM0_CTL,0);
+
+}
+
+u32 TIMER0A_u32MeasureTimerPeriod(void)
+{
+int risingEdge, fallingEdge;
+	  /* detect first rising edge */
+    TIMER0->ICR = 4;            /* clear timer0A capture flag */
+    while((TIMER0->RIS & (1<<2)) == 0) ;    /* wait till first positive edge captured */
+    risingEdge = TIMER0->TAR;     /* save the timestamp */
+		/* detect second rising edge */
+    TIMER0->ICR = 4;            /* clear timer0A capture flag */
+    while((TIMER0->RIS & (1<<2)) == 0) ;    /* wait till second positive edge captured*/
+    fallingEdge = TIMER0->TAR;     /* save the timestamp */
+		return (fallingEdge - risingEdge) & 0x00FFFFFF; /* return the time difference which is time period */	
 }
