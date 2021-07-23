643a6fc60a36dba9a77dc303ce87b3c2139e4669
Mohammed Ahmed <mohamedstarwars@gmail.com>
2021-07-23 07:04:54 +0200

    Tiva-C: using unsigned qualifier in Macros.h

--- tiva-c/lib/Macros.h
+++ tiva-c/lib/Macros.h
@@ -2,7 +2,7 @@
 #define _MACROS_H
 
 #define SET_BIT(reg,bit) reg |= (1<<bit)
-#define CLEAR_BIT(reg,bit) reg &= ~(1<<bit)
+#define CLEAR_BIT(reg,bit) reg &= ~(1u<<bit)
 #define GET_BIT(reg,bit) ((reg>>bit)&1)
 #define TOGGLE_BIT(reg,bit) reg ^= (1<<bit)
 #endif
