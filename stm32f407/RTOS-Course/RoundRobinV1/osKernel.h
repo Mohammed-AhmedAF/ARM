148f45cb166bd10a82f617f1cabe057dd58104bb
Mohammed Ahmed <mohamedstarwars@gmail.com>
2021-10-20 23:21:31 +0200

    STM32F407: Adding osThreadYield

--- stm32f407/RTOS-Course/RoundRobinV1/osKernel.h
+++ stm32f407/RTOS-Course/RoundRobinV1/osKernel.h
@@ -1,11 +1,12 @@
 #ifndef _OS_KERNEL_H
 #define _OS_KERNEL_H
+#include "stm32f4xx.h"                  // Device header
 
 #include <stdint.h>
-
 void osKernelLaunch(uint32_t quanta);
 void osKernelInit(void);
 void osKernelStackInit(int i);
+void osThreadYield(void);
 
 uint8_t osKernelAddThreads(void (*task0) (void),
 														void (*task1)(void),
