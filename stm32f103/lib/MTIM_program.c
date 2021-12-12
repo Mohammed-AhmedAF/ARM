5851d314c9e9f59b3c61f62873fe9fa56f4911fe
Mohammed Ahmed <4983317+Mohammed-AhmedAF@users.noreply.github.com>
2021-12-12 21:24:04 +0200

    STM32: Update timer driver

--- stm32f103/lib/MTIM_program.c
+++ stm32f103/lib/MTIM_program.c
@@ -14,7 +14,7 @@ void MTIM_vidInit() {
 	SET_BIT(MTIM2->CR1,MTIM_CR1_CEN);
 	CLEAR_BIT(MTIM2->CR1,MTIM_CR1_DIR);
 	MTIM2->PSC = 1000; /*Setting prescaler*/
-	MTIM3->ARR = 1000000;
+	MTIM2->ARR = 1000000;
 }
 
 u16 MTIM_u16GetCount(void) {
