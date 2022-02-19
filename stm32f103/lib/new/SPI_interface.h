59d7fcfa3bc22b9c2cecad2344d3fbd8a512d410
Mohammed Ahmed <mohamedstarwars@gmail.com>
2022-02-19 15:02:05 +0200

    STM32F103: Adding struct to configure SPI

--- stm32f103/lib/new/SPI_interface.h
+++ stm32f103/lib/new/SPI_interface.h
@@ -1,7 +1,34 @@
 #ifndef _SPI_INTERFACE_H
 #define _SPI_INTERFACE_H
 
-void SPI_vidInit(void);
+typedef struct
+{
+	u8 u8Config;
+	u8 u8BaudrateControl;
+	u8 u8FrameFormat;
+	u8 u8ClockPolarity;
+	
+}SPIConfig_t;
+
+void SPI_vidInit(SPIConfig_t * config);
 void SPI_vidTransmitByte(u8 u8Byte);
+
+#define SPI_CONFIG_MASTER 1
+#define SPI_CONFIG_SLAVE 0
+
+#define SPI_CLK_POLARITY_0 0
+#define SPI_CLK_POLARITY_1 1
+
+#define SPI_FRAMEFORMAT_MSB 0
+#define SPI_FRAMEFORMAT_LSB 1
+
+#define SPI_BAUDRATE_DIV_2 0
+#define SPI_BAUDRATE_DIV_4 1
+#define SPI_BAUDRATE_DIV_8 2
+#define SPI_BAUDRATE_DIV_16 3
+#define SPI_BAUDRATE_DIV_32 4
+#define SPI_BAUDRATE_DIV_64 5
+#define SPI_BAUDRATE_DIV_128 6
+#define SPI_BAUDRATE_DIV_256 7
 
 #endif
--- stm32f103/lib/new/SPI_program.c
+++ stm32f103/lib/new/SPI_program.c
@@ -4,29 +4,79 @@
 #include "SPI_interface.h"
 #include "stm32f10x.h"                  // Device header
 
-void SPI_vidInit(void)
+void SPI_vidInit(SPIConfig_t * config)
 {
-
-
-	/*Master-slave select*/
-	SET_BIT(SPI1_CR1,2);
+	/*Configuration*/
+	switch(config->u8Config)
+	{
+		case SPI_CONFIG_MASTER:
+			SPI1->CR1 |= 0x4; // Master Mode
+			break;
+		case SPI_CONFIG_SLAVE:
+			CLEAR_BIT(SPI1->CR1,2);
+			break;
+	}
 	
-	/*SS Output enable*/
-	SET_BIT(SPI1_CR2,2);
-	
-	/*Frequeny*/
-	SET_BIT(SPI1_CR1,3);
-	SET_BIT(SPI1_CR1,4);
-	SET_BIT(SPI1_CR1,5);
+	/*Baudrate selection*/
+	switch(config->u8BaudrateControl)
+	{
+		case SPI_BAUDRATE_DIV_2:
+			SPI1->CR1 |= 0<<3;
+			break;
+		case SPI_BAUDRATE_DIV_4:
+			SPI1->CR1 |= 1<<3;
+			break;
+		case SPI_BAUDRATE_DIV_8:
+			SPI1->CR1 |= 2<<3;
+			break;
+		case SPI_BAUDRATE_DIV_16:
+			SPI1->CR1 |= 3<<3; // fclk / 16
+			break;
+		case SPI_BAUDRATE_DIV_32:
+			SPI1->CR1 |= 4<<3; // fclk / 32
+			break;
+		case SPI_BAUDRATE_DIV_64:
+			SPI1->CR1 |= 5<<3; // fclk / 64
+			break;
+		case SPI_BAUDRATE_DIV_128:
+			SPI1->CR1 |= 6<<3; // fclk / 128
+			break;
+		case SPI_BAUDRATE_DIV_256:
+			SPI1->CR1 |= 7<<3; // fclk / 265
+			break;
+	}
 
-	/*Enable SPI*/
-	//SET_BIT(SPI1_CR1,6);
+	/*Clock polarity*/
+	switch(config->u8ClockPolarity)
+	{
+		case SPI_CLK_POLARITY_0:
+			CLEAR_BIT(SPI1->CR1,1);
+			break;
+		case SPI_CLK_POLARITY_1:
+			SET_BIT(SPI1->CR1,1);
+			break;
+	}
 	
-SPI1->CR1 |= 0x4; // Master Mode
-SPI1->CR1 |= 0x31; // fclk / 265
-SPI1->CR2 |= 0x4;
-SPI1->CR1 |= 0x40; 
-//	
+	/*Frame format*/
+	switch(config->u8FrameFormat)
+	{
+		case SPI_FRAMEFORMAT_LSB:
+			SET_BIT(SPI1_CR1,7);
+			break;
+		case SPI_FRAMEFORMAT_MSB:
+			CLEAR_BIT(SPI1_CR1,7);
+			break;
+	}
+	
+	/*SS enabled*/
+	SPI1->CR2 |= 0x4;
+	
+	/*SPI enabled*/
+	SET_BIT(SPI1->CR1,6); 
+
+	/*Enabling SPI*/
+	SPI1->CR1 |= 0x40; 	
+
 }
 
 void SPI_vidTransmitByte(u8 u8Byte)
