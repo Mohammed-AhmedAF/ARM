55ee19fccafaf5039fd1540333e7ce0b8d5b1da2
Mohammed Ahmed <mohamedstarwars@gmail.com>
2022-03-02 20:17:07 +0200

    Tiva-C: Updating ButtonDebouncing example

--- tiva-c/development/ButtonDebouncing/BUTTON_interface.h
+++ tiva-c/development/ButtonDebouncing/BUTTON_interface.h
@@ -1,9 +1,16 @@
 #ifndef _BUTTON_INTERFACE_H
 #define _BUTTON_INTERFACE_H
 
+typedef struct
+{
+	u8 u8Port;
+	u8 u8Pin;
+}ButtonConfig_t;
+
 enum ButtonState {BUTTON_UP, BUTTON_DOWN, BUTTON_PRESSED, BUTTON_RELEASED};
 void delay_ms(int milli);
-enum ButtonState Button_u8GetButtonDebounced(enum ButtonState buttonState);
+enum ButtonState Button_u8GetButtonDebounced(ButtonConfig_t * buttonConfig,
+	enum ButtonState buttonState);
 
 
 #endif
