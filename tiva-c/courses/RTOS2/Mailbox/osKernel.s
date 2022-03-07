0f0c9a681925c12ea6932932d1c1515569630006
Mohammed Ahmed <4983317+Mohammed-AhmedAF@users.noreply.github.com>
2022-03-07 12:37:35 +0200

    Tiva-C
    Completing comments in Mailbox example

--- tiva-c/courses/RTOS2/Mailbox/osKernel.s
+++ tiva-c/courses/RTOS2/Mailbox/osKernel.s
@@ -7,15 +7,15 @@
 		
 PendSV_Handler ;saved automatically: R0,R1,R2,R3,R12,lr,pc,psr
 	CPSID I ;Disable interrupts
-	PUSH {R4-R11} ;Save R4,R5,R6,R7,R8,R9,R10,R11
+	PUSH {R4-R11} ;Save R4,R5,R6,R7,R8,R9,R10,R11 onnto the stack
 	LDR R0,=currentPt ;Load the TCB of thread about to be suspended
 	LDR R1,[R0] ;R1 = currentPt
-	STR SP,[R1] ;Store the stack pointer of processor into the stack pointer of the thread to suspended
+	STR SP,[R1] ;Store the stack pointer of processor into the stack pointer of the thread to be suspended
 	;LDR R1,[R1,#4] ;R1 = next TCB to be switched to
 	;STR R1,[R0] ;Store value of R1, next of TCB, into the address of R0 which holds currentPt
 	PUSH {R0,LR}
 	BL osPeriodicScheduler
-	POP {R0,LR}
+	POP {R0,LR} ;Load registers from the stack into register list
 	LDR R1,[R0] ;R1 = currentPt i.e new thread
 	LDR SP,[R1] ;SP is equal to the stack pointer of the new thread (switched to)
 	POP {R4-R11} ;Restore the registers we pushed into the stack
