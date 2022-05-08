96f80514f4f4037e3820c67b35a23f3e814cd026
Mohammed Ahmed <4983317+Mohammed-AhmedAF@users.noreply.github.com>
2022-05-08 08:28:48 +0000

    Tiva-C: correcting comment in RTOS2 course

--- tiva-c/courses/RTOS2/RoundRobin_v1/osKernel.s
+++ tiva-c/courses/RTOS2/RoundRobin_v1/osKernel.s
@@ -9,7 +9,7 @@ SysTick_Handler ;saved automatically: R0,R1,R2,R3,R12,lr,pc,psr
 	PUSH {R4-R11} ;Save R4,R5,R6,R7,R8,R9,R10,R11
 	LDR R0,=currentPt ;Load the TCB of thread about to be suspended
 	LDR R1,[R0] ;R1 = currentPt
-	STR SP,[R1] ;Store the stack pointer of processor into the stack pointer of the thread to suspended
+	STR SP,[R1] ;Store the stack pointer of processor into the stack pointer of the thread to be suspended
 	LDR R1,[R1,#4] ;R1 = next TCB to be switched to
 	STR R1,[R0] ;Store value of R1, next of TCB, into the address of R0 which holds currentPt
 	LDR SP,[R1] ;SP is equal to the stack pointer of the new thread (switched to)
