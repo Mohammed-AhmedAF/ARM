ad4f7c59ffb47e6fce9a59c3164fd112525dfe25
Mohammed Ahmed Abd Al-Fattah <mohamedstarwars@gmail.com>
2022-06-07 02:23:47 +0200

    Tiva-C: Adding const to register definition in SPI driver

--- tiva-c/lib/SPI_private.h
+++ tiva-c/lib/SPI_private.h
@@ -4,12 +4,12 @@
 /*SPI0*/
 
 /*SPI1*/
-#define SSICR0 * ((u32 *) 0x40009000)
-#define SSICR1 * ((u32 *) 0x40009004)
-#define SSIDR * ((u32 *) 0x40009008)
-#define SSISR * ((u32 *) 0x4000900C)
-#define SSICPSR * ((u32*) 0x40009010)
-#define SSICC * ((u32*) 0x40009FC8)
+#define SSICR0 * ((u32 * const) 0x40009000)
+#define SSICR1 * ((u32 * const) 0x40009004)
+#define SSIDR * ((u32 * const) 0x40009008)
+#define SSISR * ((u32 * const) 0x4000900C)
+#define SSICPSR * ((u32 * const) 0x40009010)
+#define SSICC * ((u32 * const) 0x40009FC8)
  
 
 #endif
