00fe738a1e929a3445c79cd23f767b2c064d06f6
Mohammed Ahmed Abd Al-Fattah <mohamedstarwars@gmail.com>
2022-06-16 01:44:47 +0200

    Tiva-C: Adding displayTwoDigitsLeft and displayTwoDigitsRight to TM13637 driver

--- tiva-c/lib/TM1637_interface.h
+++ tiva-c/lib/TM1637_interface.h
@@ -16,5 +16,17 @@ void TM1637_vidDisplayDecimal(u32 u32Val, u8 u8DisplaySeparator);
 void TM1637_vidSetBrightness(u8 u8BrightnessLevel);
 void TM1637_vidDelayM(u32 u32DelayVal);
 void TM1637_vidDisplayChar(u8 u8Char);
+void TM1637_vidDisplayTemperatur(u32 u32Val, u8 u8DisplaySeparator);
+void TM1637_vidDisplayDigit(u8 u8Digit, u8 u8Position, u8 u8DisplayPosition);
+void TM1637_vidDisplayTwoNumbersRight(u8 u8Number,u8 u8DisplaySeparator);
+void TM1637_vidDisplayTwoNumbersLeft(u8 u8Number);
+
+/*Macros*/
+#define TM1637_SEPARATOR_DISABLED 0
+#define TM1637_SEPARATOR_ENABLED 1
+#define TM1637_POS_0 0
+#define TM1637_POS_1 1
+#define TM1637_POS_2 2
+#define TM1637_POS_3 3
 
 #endif
--- tiva-c/lib/TM1637_program.c
+++ tiva-c/lib/TM1637_program.c
@@ -3,14 +3,12 @@
 #include "GPIO_interface.h"
 #include "TM1637_config.h"
 #include "TM1637_interface.h"
-#include <stdint.h>
-#include <stdbool.h>
-#include "driverlib/sysctl.h"
+#include "TIMER0_interface.h"
 
 const u8 segmentMap[] = {
     0x3f, 0x06, 0x5b, 0x4f, 0x66, 0x6d, 0x7d, 0x07, // 0-7
     0x7f, 0x6f, 0x77, 0x7c, 0x39, 0x5e, 0x79, 0x71, // 8-9, A-F
-    0x00
+    0x00 
 };
 
 void TM1637_vidInit(void)
@@ -24,6 +22,11 @@ void TM1637_vidInit(void)
 	GPIO_vidSetPinDigEnable(TM1637_CLK_PORT,TM1637_CLK_PIN,GPIO_DEN_SET);
 	
 	TM1637_vidSetBrightness(8);
+	
+	TM1637_vidDisplayDigit(0,TM1637_POS_0,TM1637_SEPARATOR_DISABLED);
+	TM1637_vidDisplayDigit(0,TM1637_POS_1,TM1637_SEPARATOR_DISABLED);
+	TM1637_vidDisplayDigit(0,TM1637_POS_2,TM1637_SEPARATOR_DISABLED);
+	TM1637_vidDisplayDigit(0,TM1637_POS_3,TM1637_SEPARATOR_DISABLED);
 }
 
 static void TM1637_vidStart(void)
@@ -102,12 +105,12 @@ static void TM1637_vidWriteByte(u8 u8Byte)
 
 static void TM1637_vidDelay(u8 u8DelayVal)
 {
-	SysCtlDelay(u8DelayVal * (SysCtlClockGet() / 3 / 1000000));
+	TIMER0_viDelayMirco(u8DelayVal);
 }
 
 void TM1637_vidDelayM(u32 u32DelayVal)
 {
-	SysCtlDelay(u32DelayVal * (SysCtlClockGet() / 3 / 1000));	
+	TIMER0_vidDelayMilli(u32DelayVal);
 }
 
 void TM1637_vidDisplayChar(u8 u8CharArr)
@@ -119,8 +122,8 @@ void TM1637_vidDisplayChar(u8 u8CharArr)
 	digitArr[2] = segmentMap[10];
 	digitArr[3] = segmentMap[15];
 
-	
 	TM1637_vidStart();
+	/*Write data to display register*/
 	TM1637_vidWriteByte(0x40);
 	TM1637_u8ReadResult();
 	TM1637_vidStop();
@@ -152,13 +155,14 @@ void TM1637_vidDisplayDecimal(u32 u32Val, u8 u8DisplaySeparator)
 		u32Val /= 10;
 	}
 	
-
-
+	digitArr[2] = 0x63; /*degree symbol*/
+	digitArr[3] = 0x39; /*C*/
+	
 	TM1637_vidStart();
+	/*Write data to display register*/
 	TM1637_vidWriteByte(0x40);
 	TM1637_u8ReadResult();
 	TM1637_vidStop();
-	
 	
 	TM1637_vidStart();
 	TM1637_vidWriteByte(0xC0);
@@ -171,17 +175,91 @@ void TM1637_vidDisplayDecimal(u32 u32Val, u8 u8DisplaySeparator)
 	}
 	
 	TM1637_vidStop();
+}
+
+void TM1637_vidDisplayTwoNumbersLeft(u8 u8Number)
+{
+	TM1637_vidDisplayDigit(u8Number/10,TM1637_POS_0,TM1637_SEPARATOR_DISABLED);
+	TM1637_vidDisplayDigit(u8Number%10,TM1637_POS_1,TM1637_SEPARATOR_DISABLED);
+}
+
+void TM1637_vidDisplayTwoNumbersRight(u8 u8Number,u8 u8DisplaySeparator)
+{
+	/*Because we are displaying 2 digits only, so the maximum is 99*/
+	if (u8Number < 100)
+	{
+		if (u8DisplaySeparator == 1)
+		{
+			TM1637_vidDisplayDigit(u8Number/10,TM1637_POS_2,TM1637_SEPARATOR_ENABLED);	
+		}
+		TM1637_vidDisplayDigit(u8Number%10,TM1637_POS_3,TM1637_SEPARATOR_DISABLED);
+	}
+}
+
+void TM1637_vidDisplayDigit(u8 u8Digit, u8 u8Position, u8 u8DisplaySeparator)
+{
+	TM1637_vidStart();
+	/*Write data to display register*/
+	TM1637_vidWriteByte(0x40);
+	TM1637_u8ReadResult();
+	TM1637_vidStop();
 	
+	TM1637_vidStart();
+	TM1637_vidWriteByte(0xC0+u8Position);
+	TM1637_u8ReadResult();
+	
+	
+	if ((u8Position == 2) && u8DisplaySeparator)
+	{
+		TM1637_vidWriteByte(segmentMap[u8Digit]|(1<<7));
+	}
+	else
+	{
+		TM1637_vidWriteByte(segmentMap[u8Digit]);
+	}
+	TM1637_u8ReadResult();
+}
+
+void TM1637_vidDisplayTemperatur(u32 u32Val, u8 u8DisplaySeparator)
+{
+u8 digitArr[4];
+	for (u8 u8DigitIndex = 0; u8DigitIndex < 4; u8DigitIndex++)
+	{
+		digitArr[u8DigitIndex] = segmentMap[u32Val%10];
+		if (u8DigitIndex == 2 && u8DisplaySeparator)
+		{
+			digitArr[u8DigitIndex] |= 1<<7;
+		}
+		u32Val /= 10;
+	}
+	
+	digitArr[3] = digitArr[1];
+	digitArr[2] = digitArr[0];
+	digitArr[1] = 0x63; /*Degree symbol °*/
+	digitArr[0] = 0x39; /*C*/
+	
+	TM1637_vidStart();
+	TM1637_vidWriteByte(0x40);
+	TM1637_u8ReadResult();
+	TM1637_vidStop();
+	
+	TM1637_vidStart();
+	TM1637_vidWriteByte(0xC0);
+	TM1637_u8ReadResult();
+	
+	for (u8 i = 0; i < 4; i++)
+	{
+		TM1637_vidWriteByte(digitArr[3-i]);
+		TM1637_u8ReadResult();
+	}
+	
+	TM1637_vidStop();
 }
 
 void TM1637_vidSetBrightness(u8 u8BrightnessLevel)
 {
-
  TM1637_vidStart();
  TM1637_vidWriteByte(0x87+u8BrightnessLevel);
  TM1637_u8ReadResult();
  TM1637_vidStop();
-
-
-
 }
