4aed5c3beb25eb20bab0eae99d944569b450cc5c
Mohammed Ahmed <mohamedstarwars@gmail.com>
2022-07-25 04:06:55 +0200

    STM32F103: Adding stm32calculator.py to lib directory

---
+++ stm32f103/lib/stm32calculator.py
@@ -0,0 +1,51 @@
+from tkinter import *
+from tkinter import ttk
+
+def calculateBaudrate():
+    systemClock = int(systemClockCmBox.get())*1000000
+    baudrate = int(baudrateCmBox.get())
+    if overSampVar.get() == 1:
+        usartdivDecimal = (systemClock)/((baudrate)*16)
+    else:
+        usartdivDecimal = (systemClock)/((baudrate)*8)
+    usartdivLabel.config(text=str(usartdivDecimal))
+
+    #Get the fraction
+    mantissa = int(usartdivDecimal)
+    print(mantissa)
+    fraction = usartdivDecimal - mantissa
+    fraction = round(fraction*16)
+    usartdivLabel.config(text=str(hex(mantissa))+str(fraction))
+
+window = Tk()
+baudrateValues = [9600,19200,38400,57600,115200]
+systemClockValues = [16,24,36,48,56,72]
+window.title("STM32 UART Calculator")
+overSampVar = IntVar()
+
+inputFrame = Frame(window)
+systemClockLabel = Label(inputFrame,text="System clock")
+systemClockCmBox = ttk.Combobox(inputFrame,values=systemClockValues,state="readonly")
+systemClockCmBox.current(0)
+baudrateLabel = Label(inputFrame,text="Baudrate ")
+baudrateCmBox = ttk.Combobox(inputFrame,values=baudrateValues,state="readonly")
+baudrateCmBox.current(0)
+calculateButton = Button(inputFrame,text="Calculate",command=calculateBaudrate)
+usartdivLabel = Label()
+overSamplingLabel = Label(inputFrame,text="Over sampling")
+overSamplingChBox = Checkbutton(inputFrame,variable=overSampVar,onvalue=1,offvalue=0)
+
+
+inputFrame.grid(row=0,column=0)
+systemClockLabel.grid(row=0,column=0,padx=10,pady=5)
+baudrateLabel.grid(row=1,column=0,padx=10,pady=5)
+baudrateCmBox.grid(row=1,column=1,padx=10,pady=5)
+systemClockCmBox.grid(row=0,column=1,padx=10,pady=5)
+overSamplingLabel.grid(row=2,column=0,padx=10,pady=5)
+overSamplingChBox.grid(row=2,column=1,padx=10,pady=5)
+calculateButton.grid(row=3,column=0,padx=10,pady=5)
+usartdivLabel.grid(row=2,column=0,padx=10,pady=10)
+
+
+
+window.mainloop()
