6b4dfb05f9306c85aea71c04aabd25c7e0a57730
Mohammed Ahmed <mohamedstarwars@gmail.com>
2022-08-31 11:33:31 +0200

    Tiva-C: Select micro delay timer function

--- tiva-c/lib/SERVO_interface.h
+++ tiva-c/lib/SERVO_interface.h
@@ -5,6 +5,7 @@ typedef struct
 {
 	u8 u8SignalPort;
 	u8 u8SignalPin;
+	void (*ptFMicroDelay) (u32);
 }ServoConfig_t;
 
 
@@ -12,4 +13,5 @@ void SERVO_vidInit(const ServoConfig_t * conf);
 void SERVO_0_Degree(const ServoConfig_t * conf);
 void SERVO_90_Degree(const ServoConfig_t * conf);
 void SERVO_180_Degree(const ServoConfig_t * conf);
+void SERVO_vidSetDelayTimerFunc(void (*ptrF) (u32));
 #endif
--- tiva-c/lib/SERVO_program.c
+++ tiva-c/lib/SERVO_program.c
@@ -1,57 +1,69 @@
 #include "Macros.h"
 #include "STD_TYPES.h"
 #include "GPIO_interface.h"
-#include "TIMER0_interface.h"
 #include "SERVO_interface.h"
+
+/*Pointer to function micro delay*/
+static void (*ptrFunMicroDelay) (u32);
 
 void SERVO_vidInit(const ServoConfig_t * conf)
 {
 	GPIO_vidSetPinDirection(conf->u8SignalPort,conf->u8SignalPin,GPIO_DIR_OUTPUT);
 	GPIO_vidSetPinDigEnable(conf->u8SignalPort,conf->u8SignalPin,GPIO_DEN_SET);
+	
+	/*Setting micro delay timer function*/
+	SERVO_vidSetDelayTimerFunc(conf->ptFMicroDelay);
+}
+
+void SERVO_vidSetDelayTimerFunc(void (*ptrF) (u32))
+{
+	ptrFunMicroDelay = ptrF;
 }
 
 void SERVO_0_Degree(const ServoConfig_t * conf)
 {
-   int i=0;  	 
+  int i=0;  	 
 	for(i=0; i<50; i++) 
 	{
 	  /* Given 10us trigger pulse */
-		GPIO_vidSetPinValue(conf->u8SignalPort,conf->u8SignalPin,1); /* make control  pin high */
-	  TIMER0_viDelayMirco(600); /*0.6ms seconds delay */
-		GPIO_vidSetPinValue(conf->u8SignalPort,conf->u8SignalPin,0); /* make control  pin high */
-	  TIMER0_viDelayMirco(19400); /*1.94ms seconds delay */
+		/*Control pin high */
+		GPIO_vidSetPinValue(conf->u8SignalPort,conf->u8SignalPin,1);
+	  ptrFunMicroDelay(600); /*0.6ms seconds delay */
+		/*Control pin low */
+		GPIO_vidSetPinValue(conf->u8SignalPort,conf->u8SignalPin,0); 
+	  ptrFunMicroDelay(19400); /*1.94ms seconds delay */
   }
 }
 
-/* This function generate a 7% duty cycle from 20ms PWM signal or 50Hz*/
+/* This function generates a 7% duty cycle from 20ms PWM signal or 50Hz*/
 void SERVO_90_Degree(const ServoConfig_t * conf)
 {
    int i=0; 
 	 for(i=0; i<50; i++) 
-    {	
-		/* Given 10us trigger pulse */
-		/* make control  pin high */
+   {	
+		/*Given 10us trigger pulse */
+		/*Control pin high */
 		GPIO_vidSetPinValue(conf->u8SignalPort,conf->u8SignalPin,1); 
-		TIMER0_viDelayMirco(1400); /*1.4ms seconds delay */
-		/* make control  pin high */
+		ptrFunMicroDelay(1400); /*1.4ms seconds delay */
+		/*Control pin high */
 		GPIO_vidSetPinValue(conf->u8SignalPort,conf->u8SignalPin,0);
-	  TIMER0_viDelayMirco(18600); /*1.86ms seconds delay */
-		}
+	  ptrFunMicroDelay(18600); /*1.86ms seconds delay */
+	}
 }
 
-/* This function generate a 12% duty cycle from 20ms PWM signal or 50Hz*/
+/* This function generates a 12% duty cycle from 20ms PWM signal or 50Hz*/
 void SERVO_180_Degree(const ServoConfig_t * conf)
 {
    	int i=0; 
 	  for(i=0; i<50; i++) 
     {
-	  /* Given 10us trigger pulse */			
-		/* make control  pin high */
-		GPIO_vidSetPinValue(conf->u8SignalPort,conf->u8SignalPin,1); 
-	  /*2.4ms seconds delay */
-		TIMER0_viDelayMirco(2400); 
-	  /* make trigger  pin high */
-		GPIO_vidSetPinValue(conf->u8SignalPort,conf->u8SignalPin,0);
-		TIMER0_viDelayMirco(17600); /*1.76ms seconds delay */
+			/*Given 10us trigger pulse */			
+			/*Control pin high */
+			GPIO_vidSetPinValue(conf->u8SignalPort,conf->u8SignalPin,1); 
+			/*2.4ms seconds delay*/
+			ptrFunMicroDelay(2400); 
+			/*Trigger pin high*/
+			GPIO_vidSetPinValue(conf->u8SignalPort,conf->u8SignalPin,0);
+			ptrFunMicroDelay(17600); /*1.76ms seconds delay */
 		}
 }
